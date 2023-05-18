8e5770640d04808002933f87c640ff5c27c25600
wangxiao <xiao.wang@gysecurity.com>
2023-05-18 16:55:28 +0800

    updata

--- python_Commonly_used_built_in_methods/funtion_divmod.py
+++ python_Commonly_used_built_in_methods/funtion_divmod.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 # -*- encoding: utf-8 -*-
 
+# 用于执行整数除法并同时返回商和余数的函数
+"""
+divmod(int,int)
+第一个是被除数
+第二个是除数
+返回一个元素 (商:余数)
+"""
 
-data = input('输入：')
-code = compile(f'{data}', '<string>', 'single')
-exec(code)
-
-
+result = divmod(10, 3)
+print(result)
